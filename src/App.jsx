04fd0abb4be22196e4eb48562f78e95b2b37074a
Ahmad Fajar Alfaravi <afajaralfaravi@gmail.com>
2025-07-07 15:28:36 +0700

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,55 @@ function App() {
       </div>
 
       {/* About */}
-      <div></div>
+      <section className="about mt-32 py-12 px-4 md:px-8 lg:px-2 ">
+        <div className="max-w-6xl mx-auto shadow-lg rounded-2xl p-6 sm:p-10 lg:p-14 transition-all duration-300 hover:shadow-xl bg-white">
+          <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center md:text-left">
+            👨‍💻 Tentang Saya
+          </h2>
+          <p className="text-justify text-base md:text-lg leading-relaxed text-gray-700">
+            👋 Halo! Saya <strong>Ahmad Fajar Alfaravi</strong>, lulusan S1
+            Sistem Informasi tahun 2023. Selama masa kuliah, saya aktif
+            mengikuti berbagai kegiatan seperti <strong>organisasi</strong>,
+            <strong> workshop</strong>, dan <strong>program magang</strong> yang
+            memperkaya wawasan dan keterampilan saya.
+            <br />
+            <br />
+            💼 Saya memiliki pengalaman magang sebagai{" "}
+            <strong>Digitalisasi Intern</strong> di
+            <strong> PT. Sanghiang Perkasa (Kalbe Group)</strong> selama hampir
+            6 bulan. Selain itu, saya juga bekerja sebagai{" "}
+            <strong>Network Staff</strong> di
+            <strong>
+              {" "}
+              Universitas Muhammadiyah Sumatera Utara (UMSU)
+            </strong>{" "}
+            selama hampir 2 tahun sambil menyelesaikan studi.
+            <br />
+            <br />
+            💻 Dalam perjalanan itu, saya terlibat langsung dalam:
+            <ul className="list-disc list-inside mt-3 mb-5 ml-4 md:ml-6">
+              <li>
+                Pembuatan dan pengembangan <strong>website</strong>
+              </li>
+              <li>
+                Pengelolaan <strong>jaringan komputer</strong>
+              </li>
+              <li>
+                Pengembangan aplikasi menggunakan <strong>Flutter</strong>
+              </li>
+            </ul>
+            🚀 Setelah lulus, saya melanjutkan karier sebagai{" "}
+            <strong>Flutter Apps Developer</strong> di UMSU selama 1 tahun
+            (kontrak).
+            <br />
+            <br />
+            📈 Saya terus bersemangat untuk{" "}
+            <strong>belajar dan berkembang</strong>, tidak hanya dalam
+            pengembangan aplikasi, tapi juga dalam bidang teknologi lainnya yang
+            relevan dengan era digital saat ini.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
