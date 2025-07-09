3324140d00ae04d908149561ca5320ea2f155114
Ahmad Fajar Alfaravi <afajaralfaravi@gmail.com>
2025-07-09 09:56:37 +0700

    add to section tools

--- src/data.js
+++ src/data.js
@@ -9,14 +9,14 @@ const Image = {
 export default Image;
 
 import Tools1 from "/assets/tools/vscode.png";
-import Tools2 from "/assets/tools/reactjs.png";
-import Tools3 from "/assets/tools/nextjs.png";
-import Tools4 from "/assets/tools/tailwind.png";
-import Tools5 from "/assets/tools/bootstrap.png";
-import Tools6 from "/assets/tools/js.png";
-import Tools7 from "/assets/tools/nodejs.png";
-import Tools8 from "/assets/tools/github.png";
-import Tools9 from "/assets/tools/ai.png";
+import Tools2 from "/assets/tools/cursorai.png";
+import Tools3 from "/assets/tools/reactjs.png";
+import Tools4 from "/assets/tools/flutter.png";
+import Tools5 from "/assets/tools/tailwind.png";
+import Tools6 from "/assets/tools/bootstrap.png";
+import Tools7 from "/assets/tools/js.png";
+import Tools8 from "/assets/tools/dart.png";
+import Tools9 from "/assets/tools/github.png";
 import Tools10 from "/assets/tools/canva.png";
 import Tools11 from "/assets/tools/figma.png";
 
@@ -31,58 +31,58 @@ export const listTools = [
   {
     id: 2,
     gambar: Tools2,
+    nama: "Cursor AI",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
     nama: "React JS",
     ket: "Framework",
     dad: "200",
   },
   {
-    id: 3,
-    gambar: Tools3,
-    nama: "Next JS",
+    id: 4,
+    gambar: Tools4,
+    nama: "Flutter Framework",
     ket: "Framework",
     dad: "300",
   },
   {
-    id: 4,
-    gambar: Tools4,
+    id: 5,
+    gambar: Tools5,
     nama: "Tailwind CSS",
     ket: "Framework",
     dad: "400",
   },
   {
-    id: 5,
-    gambar: Tools5,
+    id: 6,
+    gambar: Tools6,
     nama: "Bootstrap",
     ket: "Framework",
     dad: "500",
   },
   {
-    id: 6,
-    gambar: Tools6,
+    id: 7,
+    gambar: Tools7,
     nama: "Javascript",
     ket: "Language",
     dad: "600",
   },
   {
-    id: 7,
-    gambar: Tools7,
-    nama: "Node JS",
-    ket: "Javascript Runtime",
-    dad: "700",
-  },
-  {
     id: 8,
     gambar: Tools8,
-    nama: "Github",
-    ket: "Repository",
-    dad: "800",
+    nama: "Dart",
+    ket: "Language",
+    dad: "600",
   },
   {
     id: 9,
     gambar: Tools9,
-    nama: "Adobe Illustrator",
-    ket: "Design App",
-    dad: "900",
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
   },
   {
     id: 10,
